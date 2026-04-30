e24a4a32bedcbfe9fe99519811fc7a8bcec76ba2
dalvyon1da <on1dal140271@gmail.com>
2026-04-30 07:46:58 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 console.log("Site ON4CPN chargé.");
 
+// Animation d’apparition progressive
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".fade-in").forEach(el => {
+        el.style.opacity = 1;
+    });
+});
